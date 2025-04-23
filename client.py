a005f9a7f747dbb76340aaec09a519813d141440
samuelhsieh0829 <66865408+samuelhsieh0829@users.noreply.github.com>
2025-04-23 16:06:02 +0800

    Client test

---
+++ client.py
@@ -0,0 +1,81 @@
+import pyaudio
+import socket
+import struct
+import sys
+import time
+import threading
+
+# Audio init
+chunk = 1024
+sample_format = pyaudio.paInt16
+channels = 1
+fs = 44100
+
+input_audio = pyaudio.PyAudio()
+audio_in = input_audio.open(format=sample_format, channels=channels, rate=fs, frames_per_buffer=chunk, input=True)
+
+output_audio = pyaudio.PyAudio()
+audio_out = output_audio.open(format=sample_format, channels=channels, rate=fs, frames_per_buffer=chunk, output=True)
+
+# Socket init
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+IP = input("Enter the IP address of the target: ")
+audio_target_location = (IP, 5001)
+s.bind(audio_target_location)
+
+def receive_audio():
+    global s
+    try:
+        print("Start receiving data")
+        while True:
+            data, addr = s.recvfrom(8192)
+
+            # Get timestamp
+            timestamp_byte = data[:8]
+            timestamp = struct.unpack(">d", timestamp_byte)[0]
+            audio_out.write(data[8:])
+
+            # Output Ping
+            t_delta = time.time() - timestamp
+            sys.stdout.write(f"\rPing: {t_delta*1000:.2f} ms")
+            sys.stdout.flush()
+
+            # Check for latency
+            if t_delta > 0.03:
+                print(f"\nWarning: High latency detected: {t_delta*1000:.2f} ms, rebinding socket")
+                s.close()
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s.bind(audio_target_location)
+    except KeyboardInterrupt:
+        print("\nCtrl + C detected")
+    finally:
+        output_audio.terminate()
+        s.close()
+        print("Stopped")
+
+def send_audio_data():
+    global s, audio_in
+    try:
+        print("Start sending data")
+        while True:
+            audio = audio_in.read(chunk)
+            # Add timestamop
+            timestamp = time.time()
+            timestamp_bytes = struct.pack(">d", timestamp)
+            s.sendto(timestamp_bytes+audio, audio_target_location)
+    except KeyboardInterrupt:
+        print("\nCtrl + C detected")
+    finally:
+        s.close()
+        input_audio.terminate()
+        print("Stopped")
+
+if __name__ == "__main__":
+    sender_thread = threading.Thread(target=send_audio_data)
+    receiver_thread = threading.Thread(target=receive_audio)
+
+    sender_thread.start()
+    receiver_thread.start()
+
+    sender_thread.join()
+    receiver_thread.join()
